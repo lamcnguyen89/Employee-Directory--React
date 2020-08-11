70e0c6c04d27755251f772a73e80a990883b0234
Lam Nguyen <lamcnguyen89@gmail.com>
2020-08-11 18:52:27 -0400

    Updates

---
+++ precache-manifest.d751967c775932844795ee5cd3c874d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3641de720a203aa73d08e0ed173e4ebe",
+    "url": "/Employee-Directory--React/index.html"
+  },
+  {
+    "revision": "2fbd1643a18535ec750d",
+    "url": "/Employee-Directory--React/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b6ccf1d6355884f6cbb4",
+    "url": "/Employee-Directory--React/static/js/2.8bb6663a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Employee-Directory--React/static/js/2.8bb6663a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fbd1643a18535ec750d",
+    "url": "/Employee-Directory--React/static/js/main.049cad55.chunk.js"
+  },
+  {
+    "revision": "270b9e74cba34f0eea3b",
+    "url": "/Employee-Directory--React/static/js/runtime-main.7c5317a0.js"
+  }
+]);
